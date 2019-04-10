216a00545426a03e827905b766ebc5d7a5dd56b5
Abbet Wang <abbetwang@gmail.com>
2019-04-10 21:10:23 +0800

    add prettier

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-(()=>{
-    console.log(33)
-})()
+(() => {
+  console.log(33);
+})();
 
-const a = new Promise(()=>{console.log(1)})
+const a = new Promise(() => {
+  console.log(1);
+});
 
-async function sleep(duration){
-    return new Promise(resolve=>{
-        setTimeout(() => {
-        resolve()
-        }, duration);
-    })
+async function sleep(duration) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, duration);
+  });
 }
-async function main(){
-    console.log("sleep")
-    await sleep(5000)
-    console.log("weak")
+async function main() {
+  console.log("sleep");
+  await sleep(5000);
+  console.log("weak");
 }
 
-main()
+main();
