52b6c60658073584066ceb6849a4ee9678b0c451
Abbet Wang <abbetwang@gmail.com>
2019-04-10 20:35:25 +0800

    add polyfill

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 (()=>{
     console.log(33)
 })()
+
+const a = new Promise(()=>{console.log(1)})
+
+async function sleep(duration){
+    return new Promise(resolve=>{
+        setTimeout(() => {
+        resolve()
+        }, duration);
+    })
+}
+async function main(){
+    console.log("sleep")
+    await sleep(5000)
+    console.log("weak")
+}
+
+main()
